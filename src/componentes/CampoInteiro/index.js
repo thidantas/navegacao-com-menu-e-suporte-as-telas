78b849b9559278606283687c43482994884e5700
Thiago Dantas <thiago.dantas@BRSAOMN030776.local>
2022-06-24 14:53:10 -0300

    feat: add button to cart

--- src/componentes/CampoInteiro/index.js
+++ src/componentes/CampoInteiro/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { TextInput } from "react-native";
+import estilosPadrao from "./estilos";
 
-export default function CampoInteiro({ valor, acao }) {
+export default function CampoInteiro({ valor, estilos, acao }) {
+  const atualiza = (novoValor, acaoRetorno) => {
+    const verificaInteiro = novoValor.match(/^[0-9]*$/);
+    if (!verificaInteiro) return;
+
+    const removeZeroEsquerda = novoValor.replace(/^(0)(.+)/, "$2");
+
+    acaoRetorno(removeZeroEsquerda);
+  };
+
   const numeroEmTexto = String(valor);
   return (
     <TextInput
+      style={[estilosPadrao.campo, estilos]}
       keyboardType="number-pad"
       selectTextOnFocus
       onChangeText={(novoValor) => {
-        acao(novoValor);
+        atualiza(novoValor, acao);
       }}
       value={numeroEmTexto}
     />
